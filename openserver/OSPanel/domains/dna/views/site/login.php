12e1bea93ae5c9b3eddb5d6e79d3cee27a6d92eb
wohorogov <36229321+wohorogov@users.noreply.github.com>
2019-04-18 21:32:36 +0500

    asd
    asd

--- openserver/OSPanel/domains/dna/views/site/login.php
+++ openserver/OSPanel/domains/dna/views/site/login.php
@@ -4,9 +4,9 @@ use yii\widgets\ActiveForm;
 ?>
 <?php $form = ActiveForm::begin();?>
 
-<?= $form->field($login_model,'email')->textInput()?>
+<?= $form->field($model,'email')->textInput(['autofocus'=>true])?>
 
-<?= $form->field($login_model,'password')->passwordInput()?>
+<?= $form->field($model,'password')->passwordInput()?>
 
 <div>
     <button class="btn btn-success" type="submit">Login</button>
